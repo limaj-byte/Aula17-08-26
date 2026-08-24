b08d29643bd012947ec3b2511cff0be42baf0af6
limaj-byte <lima.j@estudante.ifro.edu.br>
2026-08-24 09:21:41 -0400

    AulaTesteCommit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+using teste_PDS.Components;
+using teste_PDS.Configs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+//configuração da conexão com o banco de dados MYSQL
+builder.Services.AddScoped<Conexao>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
